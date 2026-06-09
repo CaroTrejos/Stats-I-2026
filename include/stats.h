a5c64949b64241582ea9cddac10d662698c1770c
Carolina Trejos <carotq777@gmail.com>
2026-06-08 19:16:38 -0600

    Agregar estructura del proyecto

---
+++ include/stats.h
@@ -0,0 +1,15 @@
+#ifndef STATS_H
+#define STATS_H
+
+/*
+ * stats.h
+ * Interfaz del módulo de estadísticas para la práctica de bibliotecas.
+ * IE-0117 - I-2026
+ */
+
+int   suma    (int *arr, int n);
+float promedio(int *arr, int n);
+int   minimo  (int *arr, int n);
+int   maximo  (int *arr, int n);
+
+#endif /* STATS_H */
