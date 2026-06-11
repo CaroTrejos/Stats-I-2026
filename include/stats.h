87b5e319a0010441c3467bafcc9ddcf1c1e678d4
Carolina Trejos <carotq777@gmail.com>
2026-06-11 17:23:22 -0600

    Cambiar tipo de dato de retorno de promedio

--- include/stats.h
+++ include/stats.h
@@ -1,14 +1,8 @@
 #ifndef STATS_H
 #define STATS_H
 
-/*
- * stats.h
- * Interfaz del módulo de estadísticas para la práctica de bibliotecas.
- * IE-0117 - I-2026
- */
-
 int   suma    (int *arr, int n);
-float promedio(int *arr, int n);
+int   promedio(int *arr, int n);
 int   minimo  (int *arr, int n);
 int   maximo  (int *arr, int n);
 
